6e627643cb47c499c2c78f6ac23ef86810a41772
isa <5445946+icortes@users.noreply.github.com>
2021-09-25 18:33:00 -0700

    made game menu start disappear when play button clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,42 @@
-//create object with question, 4 options, and correct answer
+//create container for game blocks
+var gameContainers = document.querySelector(".container");
+//create container for play button
+var playButton = gameContainers.querySelector("#playGameBtn");
 
-function Question(question, option1, option2,option3,option4, answer){
-    this.myQuestion = question;
-    this.op1 = option1;
-    this.op2 = option2;
-    this.op3 = option3;
-    this.op4 = option4;
-    this.correctAns = answer;
+//check if play button is clicked
+playButton.addEventListener("click", function(event){
+    
+    var element = event.target;
+    console.log(element);
+    
+    //get parent and parent data-state
+    var parent = element.parentNode;
+    var parentState = element.parentNode.getAttribute("data-state");
+
+    console.log(parentState, typeof parentState);
+
+    // if the start menu is visible hide it
+    if(parentState === "visible"){
+
+        parent.setAttribute("data-state", "hidden");
+        parent.setAttribute("style", "display: none");
+    }
+});
+
+
+//create object with question, 4 options, and correct answer
+class Question {
+    constructor(question, option1, option2, option3, option4, answer) {
+        this.myQuestion = question;
+        this.op1 = option1;
+        this.op2 = option2;
+        this.op3 = option3;
+        this.op4 = option4;
+        this.correctAns = answer;
+    }
+}
+
+//load game
+function loadGame() {
+
 }
