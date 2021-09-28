4fdc0daa0d4b281cb6e633bb4a57329e4d5995c3
isa <5445946+icortes@users.noreply.github.com>
2021-09-27 20:02:03 -0700

    highscores now show up to the user

--- assets/js/script-hs.js
+++ assets/js/script-hs.js
@@ -1,1 +1,32 @@
 //get highscore list
+var hsListEl = document.getElementById("hs-list");
+
+//function to show highscores
+function showHighScores() {
+    //clear hsListEl
+    hsListEl.innerHTML = "";
+    //get list of users from local storage
+    var users = JSON.parse(localStorage.getItem("users"));
+    //if users has content inside
+    if (users !== null) {
+        //for loop to append items in users to hs-list
+        for (var i = 0; i < users.length; i++) {
+            //grab item in users by index of loop
+            var item = users[i];
+            console.log(item);
+            //create li element
+            var li = document.createElement("li");
+            //add text to li element
+            li.textContent = item.init + " scored " + item.scr;
+            console.log(li);
+            //append li to hsListEl
+            hsListEl.appendChild(li);
+        }
+    } 
+    //if users doesn't have items exit function
+    else {
+        return;
+    }
+}
+
+showHighScores();
